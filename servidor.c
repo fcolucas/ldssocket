cb7a095108e1871d980f3d7370af0795d7e6b1cf
Francisco Lucas <fcolucaslima14@gmail.com>
2018-01-09 11:59:16 -0300

    Alteração servidor

--- servidor.c
+++ servidor.c
@@ -7,31 +7,70 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define PORTA 2000
+#define PORT 2000
 #define TAM 256
 
 struct sockaddr_in local;
 struct sockaddr_in remoto;
 
+int create_socket(){
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd != -1){
+        printf("Socket criado!\n");
+        return sockfd;
+    }else{
+        perror("socket ");
+        return 0;
+    }
+}
+
+void setting_socket(struct sockaddr_in *local, int family, int port){
+    local->sin_family = family;
+    local->sin_port = htons(port);
+    memset(local->sin_zero, 0x0, 8);
+}
+
+int connect_socket(int sockfd, struct sockaddr_in local, struct sockaddr_in remoto){
+    int on = bind(sockfd, (struct sockaddr *)&local, sizeof(local));
+    if(on == -1){
+        perror("bind ");
+        return 0;
+    }
+
+    on = listen(sockfd, 1);
+    if(on == -1){
+        perror("listen ");
+        return 0;
+    }
+    printf("Aguardando cliente...\n");
+
+    socklen_t len = sizeof(remoto);;
+    int client = accept(sockfd, (struct sockaddr *)&remoto, &len);
+    if(client==-1){
+        perror("accept ");
+        exit(1);
+    }
+    return client;
+	printf("Cliente conectado!\n\n");
+}
+
 int main()
 {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    socklen_t len = sizeof(remoto);
     int client, rec, env;
     char mensagem[TAM], resposta[TAM];
 
-    if(sockfd == -1){
-        perror("socket ");
-        exit(1);
-    }else{
-        printf("Socket criado!\n");
-    }
+    int sockfd = create_socket();
 
+    setting_socket(&local, AF_INET, PORT);
+/*
     local.sin_family = AF_INET;
     local.sin_port = htons(PORTA);
-    /*local.sin_addr.s_addr = inet_addr("192.168.254.1");*/
+    local.sin_addr.s_addr = inet_addr("192.168.254.1");
     memset(local.sin_zero, 0x0, 8);
+*/
+    client = connect_socket(sockfd, local, remoto);
 
+/*
     if (bind(sockfd, (struct sockaddr *)&local, sizeof(local))==-1){
         perror("bind ");
         exit(1);
@@ -50,19 +89,20 @@ int main()
         exit(1);
     }
 	printf("Cliente conectado!\n\n");
+*/
 
     do{
         rec = recv(client, resposta, 256, 0);
         resposta[rec] = '\0';
         printf("Cliente: %s\n", resposta);
 
-        //printf("Servidor: ");
+        /*printf("Servidor: ");*/
         fgets(mensagem, 256, stdin);
         mensagem[strlen(mensagem)-1] = '\0';
         env = send(client, mensagem, strlen(mensagem), 0);
 
-        //if(resposta[0]=='*') rec = -1;
-        //if(mensagem[0]=='*') env = -1;
+        /*if(resposta[0]=='*') rec = -1;
+        if(mensagem[0]=='*') env = -1;*/
 	}while(rec != -1 && env != -1);
 
 	close(client);
@@ -70,4 +110,3 @@ int main()
     printf("Servidor Encerrado! \n");
     return 0;
 }
-
